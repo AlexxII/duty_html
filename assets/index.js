99509ec74656c157efbe3a9eefebdc0fc268f439
AlexxII <murmus51@yandex.ru>
2026-01-31 16:16:41 +0300

    update style, add clock

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,19 @@
 const grid = document.getElementById("grid");
 
+function get_time() {
+  let time = new Date();
+  let hour = time.getHours().toString().padStart(2, "0");
+  let minute = time.getMinutes().toString().padStart(2, "0");
+  let seconds = time.getSeconds().toString().padStart(2, "0");
+  let timeString = hour + ":" + minute + ":" + seconds;
+
+  const clock = document.getElementById("clock");
+  clock.innerHTML = `${timeString}`;
+  clock.className = "";
+}
+
+setInterval(get_time, 1000);
+
 window.SCENARIOS.forEach(s => {
   const a = document.createElement("a");
   a.className = "tile " + s.color;
